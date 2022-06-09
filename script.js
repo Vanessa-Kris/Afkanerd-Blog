9ac9dd5a952a17c6d46eee6055ac17c144704b3b
Minion <nessakristo@gmail.com>
2022-06-09 16:28:12 +0100

    added pages

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-const searchInput = document.getElementById("searchInput");
+function myFunction() {
 
-// store name elements in array-like object
-const titlesFromDOM = document.getElementsByClassName("title");
+    var input, filter, swob, website, h2, h6, txtvalue;
+    input = document.getElementById('myInput');
+    filter = input.ariaValueMax.toUpperCase();
+    swob = document.getElementById("swob");
+    website = document.getElementById("website");
 
-// listen for user events
-searchInput.addEventListener("keyup", (event) => {
-    const { value } = event.target;
     
-    // get user search input converted to lowercase
-    const searchQuery = value.toLowerCase();
-    
-    for (const titleElement of titlesFromDOM) {
-        // store name text and convert to lowercase
-        let title = titleElement.textContent.toLowerCase();
-        
-        // compare current name to search input
-        if (title.includes(searchQuery)) {
-            // found name matching search, display it
-            nameElement.style.display = "block";
-        } else {
-            // no match, don't display name
-            titleElement.style.display = "none";
-        }
-    }
-});
+}
